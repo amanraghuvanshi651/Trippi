345b45a3f80635af1fe810d8d98b9723791bd807
Aman Raghuvanshi <amanraghuvanshi651@gmail.com>
2023-07-31 12:23:27 +0530

    added image picker

--- Trippi/ViewControllers/CreateTrip/CreateTripViewController.swift
+++ Trippi/ViewControllers/CreateTrip/CreateTripViewController.swift
@@ -12,6 +12,8 @@ class CreateTripViewController: UIViewController {
 
     //MARK: - Outlets
     @IBOutlet weak var addImageContainerView: UIView!
+    @IBOutlet weak var tripImageView: UIImageView!
+    @IBOutlet weak var addButton: UIButton!
     
     @IBOutlet weak var topSmallView: UIView!
     @IBOutlet weak var containerView: UIView!
@@ -31,5 +33,39 @@ class CreateTripViewController: UIViewController {
     
     @objc func didTapBackGroundView() {
         view.endEditing(true)
+    }
+    
+    func setUpImageView(image: UIImage) {
+        addButton.setTitle("", for: .normal)
+        addButton.setImage(nil, for: .normal)
+        tripImageView.image = image
+    }
+    
+    //MARK: - Actions
+    @IBAction func onClickAddImageButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+}
+
+//MARK: - Image Picker Delegate
+extension CreateTripViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        dismiss(animated: true) {
+            guard let image = info[.originalImage] as? UIImage else { return }
+            self.setUpImageView(image: image)
+            let imageName = UUID().uuidString
+//            let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+//
+//            if let jpegData = image.jpegData(compressionQuality: 0.8) {
+//                try? jpegData.write(to: imagePath)
+//            }
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
 }
